b656ca91829056d74b36fb961693671625188f17
notDuck <daylenfrancis908@gmail.com>
2024-08-14 16:12:06 -0400

    Initial commit

---
+++ src/test/java/org/example/testBubbleSort.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class testBubbleSort {
+
+    @Test
+    public void testSimpleFunctions() {
+
+    }
+}
